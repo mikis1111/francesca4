5f74ca4bbc48a650aa9a2dd7ee2f3af8d4bfbc04
mikis1111 <michelesaladino11@gmail.com>
2026-01-06 22:25:47 +0100

    fixed flash messagge double

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,23 @@
 class SessionsController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
-    def new
-    end
-  
-    def create
-      user = User.find_by(email: params[:email])
-  
-      if user&.authenticate(params[:password])
-        session[:user_id] = user.id
-        flash[:success] = "Bentornato, #{user.name}!"
-        redirect_to root_path
-      else
-        flash.now[:error] = "Email o password errati."
-        render :new, status: :unprocessable_entity
-      end
-    end
-  
-    def destroy
-      session[:user_id] = nil
-      flash[:success] = "Hai effettuato il logout."
-      redirect_to root_path
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Accesso effettuato con successo"
+    else
+      flash.now[:alert] = "Email o password non corrette"
+      render :new, status: :unprocessable_entity
     end
   end
-  
+
+  def destroy
+    reset_session
+    redirect_to login_path, notice: "Logout effettuato"
+  end
+end
