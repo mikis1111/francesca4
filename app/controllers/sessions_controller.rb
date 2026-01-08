34bd3f13ac4fa6e7f2a55758f18cd9805b67b6d8
mikis1111 <michelesaladino11@gmail.com>
2026-01-08 13:59:40 +0100

    fixed navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
 
   def new
-    @hide_navbar = true
-    @hide_footer = true
   end
-  
 
   def create
     user = User.find_by(email: params[:email])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,9 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
   
-  def new
-    @user = User.new
-    @hide_navbar = true
-    @hide_footer = true
-  end
-  
+    def new
+      @user = User.new
+    end
   
     def create
       @user = User.new(user_params)
